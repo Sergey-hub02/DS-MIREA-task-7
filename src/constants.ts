5331770433a2a7a94245b2d3ea0b4c266b9cce43
Sergey-hub02 <s.park190802@gmail.com>
2021-04-23 17:42:38 +0300

    Добавил файл constants.ts, содержащий глобальные константы (по типу пути к файлу с входными данными и т.д.); использовал модуль fs для чтения входных данных из файла

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+/* Название файла с входными данными */
+export const _INPUT_FILE_NAME_: string = "input.txt";
+
+/* Путь к файлу с входными данными */
+export const _PATH_TO_INPUT_FILE_: string = "/home/ezhik/Documents/DS_MIREA/DS-MIREA-task-7/input/";
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
+import * as fs from "fs";
+
+import * as vars from "./constants";
 import { generateArray } from "./modules/functions";
+
+
+/* Подобие стандартного потока ввода */
+const _STDIN_: string = fs.readFileSync(vars._PATH_TO_INPUT_FILE_ + vars._INPUT_FILE_NAME_, "utf-8");
+
 
 /**
  * Основная функция
  */
 const main = async (): Promise<void> => {
-  const testArray: Array<number> = generateArray(10, 5, 12);
-  console.log(testArray);
+  const arrayLength: number = +_STDIN_.split("\n")[0];
+  const [min, max] = _STDIN_.split("\n")[1].split(" ").map(item => + item);
+
+  const array: Array<number> = generateArray(arrayLength, min, max);
+  console.log(array);
 }
 
 
